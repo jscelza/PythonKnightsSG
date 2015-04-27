c910d1fac33b3e2d7738f67af92d12daa9993eb5
Jeff Scelza <jeffscelza76@gmail.com>
2015-04-27 00:21:01 -0600

    Cleaning up lint error dectect by Anaconda.  Added Documentation around Editor used

--- README.md
+++ README.md
@@ -12,3 +12,50 @@ We’ll be using Mark Pilgrim’s Dive Into Python3 for our study material.  We 
 * https://docs.python.org/3.5/tutorial/index.html
 
 Games with Python:  http://codecombat.com/
+
+## Development Environment
+
+* [Sublime Text 3](http://www.sublimetext.com/3)
+* Plug-ins
+  * [Anaconda](https://packagecontrol.io/packages/Anaconda)
+  * [Python Improved](https://packagecontrol.io/packages/Python%20Improved)
+* Project Configurations
+
+```
+{
+  "build_systems":
+  [
+    {
+      "file_regex": "^[ ]*File \"(...*?)\", line ([0-9]*)",
+      "name": "Anaconda Python Builder",
+      "selector": "source.python",
+      "shell_cmd": "/opt/boxen/homebrew/Cellar/python3/3.4.3/bin/python3 -u \"$file\""
+    }
+  ],
+  "folders":
+  [
+    {
+      "path": "~/repos/PythonKnightsSG"
+    }
+  ],
+  "settings":
+  {
+    "anaconda_gutter_marks": true,
+    "anaconda_gutter_theme": "alpha",
+    "anaconda_linter_mark_style": "outline",
+    "anaconda_linting": true,
+    "anaconda_linting_behaviour": "always",
+    "enable_signatures_tooltip": true,
+    "extra_paths":
+    [
+      "~/repos/PythonKnightsSG/week01"
+    ],
+    "merge_signatures_and_doc": true,
+    "pep257": true,
+    "python_interpreter": "/opt/boxen/homebrew/Cellar/python3/3.4.3/bin/python3",
+    "test_command": "python3 -m unittest discover \"$file\"",
+    "use_pylint": false,
+    "validate_imports": true
+  }
+}
+```
--- week01/funWithSys.py
+++ week01/funWithSys.py
@@ -1,38 +1,41 @@
-"""Playing with sys by using examples from Chapter 1
+"""Playing with sys by using examples from Chapter 1.
 
-  http://www.diveintopython3.net/your-first-python-program.html
+http://www.diveintopython3.net/your-first-python-program.html
 
-  Available Functions"
-  printSyspath()
-    Print value of sys.path
+Available Functions"
+printSyspath()
+  Print value of sys.path
 
-  addDirToSyspath(string)
-    Adds string to sys.path
+addDirToSyspath(string)
+  Adds string to sys.path
 
 """
 
 import sys
 
-def printSyspath():
-  """Prints sys.path value"""
-  print("sys.path has the following value:")
-  print(sys.path)
-  print()
 
-def addDirToSyspath(pythonKnightPath='./'):
-  """Add a directory to sys.path"""
-  print("Adding", pythonKnightPath, " to path")
-  print()
-  sys.path.insert(0, pythonKnightPath)
+def diplay_syspath():
+    """Print sys.path value."""
+    print("sys.path has the following value:")
+    print(sys.path)
+    print()
+
+
+def add_dir_to_Syspath(pythonKnightPath='./'):
+    """Add a directory to sys.path."""
+    print("Adding", pythonKnightPath, " to path")
+    print()
+    sys.path.insert(0, pythonKnightPath)
+
 
 if __name__ == '__main__':
-  print('Default Value')
-  printSyspath()
+    print('Default Value')
+    diplay_syspath()
 
-  addDirToSyspath("~/repos/PythonKnightsSG")
-  print('Value after inserting of directory')
-  printSyspath()
+    add_dir_to_Syspath("~/repos/PythonKnightsSG")
+    print('Value after inserting of directory')
+    diplay_syspath()
 
-  addDirToSyspath()
-  print('Value after inserting of directory')
-  printSyspath()
+    add_dir_to_Syspath()
+    print('Value after inserting of directory')
+    diplay_syspath()
