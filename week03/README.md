06e73cd2e96bf4ff2ef1151ba867d3ae8b4348f5
Jeff Scelza <jeffscelza76@gmail.com>
2015-05-11 19:50:57 -0600

    ADding Week03

--- week03/README.md
+++ week03/README.md
@@ -6,9 +6,15 @@
 
 ## Notes from Readings - Chapter 3
 
+* 3.2 Working with files and directories
+  * See Function secion below around modules
+* 3.3 List Comprehensions
+*
 
 ## Further Readings
 
+* https://docs.python.org/3.1/library/os.html
+*
 
 ## Learn Python the Hardway
 
@@ -16,3 +22,17 @@
 * Location for this weeks exercise
 
 ## Helpful Function found in reading
+
+* `import os.path` module
+  * `os.path.join("dir","filename")` -->  constructs a pathname out of one or more partial pathnames
+  * `os.path.expanduser('~')` --> expands directory variable
+  * `os.path.split('pathname')` --> creates a tulple of filename and directory
+  * `os.path.splitext('filename')` --> creates a tuple of shortname and extension.
+  * `os.path.realpath('filename')` --> using the obsolute path and filename
+* `import os` module
+  * `os.getcwd()` --> get current working directory
+  * `os.chdir('/directory')` --> Change directory
+  * `os.stat('filename')` --> returns an object that contains several different types of metadata about the file
+* `import glob` module
+  * `glob.glob('filename regex')`  --> list of files matching the regex
+* `time.localtime(time)` --> converts from Epoch to structure format.
---
+++ week03/playingCards.py
@@ -0,0 +1,79 @@
+"""Setting up Deck and Dealing cards out.
+
+Available Functions
+deal_hand(dict,list,int)
+    goes through dictionary of cards and
+find_available_card(dict)
+    goes thru deck and finds the first with owner pile
+set_up_deck()
+    create a deck of cards
+    returns a dictionary of each cards and the owner
+print_hand(dict,list)
+    Takes a dictionary of a deck and list of players
+    and prints out each hand.
+"""
+
+
+def deal_hand(deck_of_cards, players, number_of_cards):
+    """Function to deal out a desk of 52 cards."""
+    # for key, value in deck.iteritems():
+    for ctr in range(0, number_of_cards):
+        for player in players:
+            card_to_assign = find_available_card(deck_of_cards)
+            deck_of_cards.update({card_to_assign: player})
+
+
+def find_available_card(deck_of_cards):
+    """Find the card that isn't owned by pile and set new owner."""
+    tmp_list = [card for card, owner in deck_of_cards.items()
+                if owner == 'pile']
+    return tmp_list[0]
+    # for card, owner in deck_of_cards.items():
+    #     if owner == 'pile':
+    #         return card
+
+
+def print_hand(deck_of_cards, players):
+    """Function to print out who owns which card."""
+    for player in players:
+        print("%s has the following cards:" % player)
+        print_hand_for_player(deck_of_cards, player)
+
+
+def print_hand_for_player(deck_of_cards, player):
+    """Function to print out hand for a certain player."""
+    print({card for card, owner in deck_of_cards.items()
+           if owner == player})
+
+
+def set_up_deck():
+    """Function to create a deck of cards."""
+    card_numbers = (
+        '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A')
+    suits = ('Spades', 'Clubs', 'Hearts', 'Diamonds')
+    deck_of_cards = dict()
+
+    for card_num in card_numbers:
+        for suit in suits:
+            # print(card_num + "_of_" + suit)
+            my_key = card_num + "_of_" + suit
+            deck_of_cards[my_key] = 'pile'
+
+    return deck_of_cards
+
+
+if __name__ == '__main__':
+    my_deck = set_up_deck()
+    print(my_deck)
+    print()
+
+    list_of_players = ['david', 'matt', 'paul', 'jeff']
+    deal_hand(my_deck, list_of_players, 7)
+    print(my_deck)
+    print()
+
+    # print_hand(my_deck, list_of_players)
+
+    player = 'matt'
+    print("What cards are does %s the have?" % player)
+    print_hand_for_player(my_deck, player)
